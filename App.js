8edc3d97f9e4354e91147d4aec0ffb3748d02d61
DelisG <delisemarciano@gmail.com>
2023-09-30 11:54:32 -0300

    :pencil: renderizaação de componente

--- App.js
+++ App.js
@@ -4,11 +4,15 @@ import Home from "./src/screens/Home";
 import Loja from "./src/screens/Loja";
 import CarrinhoCompras from "./src/screens/CarrinhoCompras";
 import Login from "./src/screens/Login";
+import Produto from "./src/screens/Produto";
+import Carousel from "./src/components/Carrousel";
 
 export default function App() {
   return (
     <SafeAreaView>
-      <Login/>
+      <Carousel/>
+      {/* <Produto/> */}
+      {/* <Login/> */}
       {/* <CarrinhoCompras/> */}
       {/* <Loja/> */}
       {/* <Home /> */}
