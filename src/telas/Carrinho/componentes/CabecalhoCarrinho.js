41d78abced397a3311c900e8afd7a4fa60513b3f
DelisG <delisemarciano@gmail.com>
2023-10-18 17:47:24 -0300

    🗂️ Componente Cabecalho Carrinho

---
+++ src/telas/Carrinho/componentes/CabecalhoCarrinho.js
@@ -0,0 +1,43 @@
+import { useNavigation } from "@react-navigation/native";
+import React, { useContext } from "react";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import AntDesignIcon from "react-native-vector-icons/AntDesign";
+import { GlobalContext } from "../../../contextos/GlobalContext";
+
+const CabecalhoCarrinho = () => {
+    const navigation = useNavigation();
+
+    const { esvaziarCarrinho } = useContext(GlobalContext);
+
+    return (
+        <View style={estilos.container}>
+            <TouchableOpacity onPress={() => { navigation.goBack() }}>
+                <AntDesignIcon name="arrowleft" size={20} color="#000000" />
+            </TouchableOpacity>
+
+            <Text style={estilos.secao}>Minhas Compras</Text>
+
+            <TouchableOpacity onPress={esvaziarCarrinho}>
+                <Text style={estilos.link}>Limpar</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+export default CabecalhoCarrinho;
+
+const estilos = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: 15
+    },
+    secao: {
+        fontSize: 18,
+        fontWeight: "bold"
+    },
+    link: {
+        color: "#FF7A00",
+    }
+})
