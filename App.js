2782a00fc49990bbc7ca8d86a88a8ac6998c656f
DelisG <delisemarciano@gmail.com>
2023-10-18 17:43:01 -0300

    :page_with_curl: Home

--- App.js
+++ App.js
@@ -1,25 +1,17 @@
-import { SafeAreaView } from "react-native";
-import TelaInicial from "./src/screens/TelaInicial";
-import Home from "./src/screens/Home";
-import Loja from "./src/screens/Loja";
-import CarrinhoCompras from "./src/screens/CarrinhoCompras";
-import Login from "./src/screens/Login";
-import Produto from "./src/screens/Produto";
-import Carousel from "./src/components/Carrousel";
-import StackRoutes from "./src/routes/StackRoutes";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import StackRotas from "./src/rotas/StackRotas";
 import { NavigationContainer } from "@react-navigation/native";
+import { CarrinhoDeComprasProvider } from "./src/contextos/GlobalContext";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <StackRoutes />
-      {/* <Carousel/> */}
-      {/* <Produto/> */}
-      {/* <Login/> */}
-      {/* <CarrinhoCompras/> */}
-      {/* <Loja/> */}
-      {/* <Home /> */}
-      {/* <TelaInicial /> */}
+      <StatusBar />
+      <CarrinhoDeComprasProvider>
+        <StackRotas />
+      </CarrinhoDeComprasProvider>
     </NavigationContainer>
   );
 }
+
