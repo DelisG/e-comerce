f472e055d517d939e9343b4c046ecf75552394ac
DelisG <delisemarciano@gmail.com>
2023-10-18 18:04:28 -0300

    ♻️ Modificação para fins de demonstração

--- src/servicos/index.js
+++ src/servicos/index.js
@@ -1,30 +1,36 @@
 import axios from "axios";
 
 const api = axios.create({
-    // Link to Postman Mock Server
-    baseURL: "https://7a46adff-85b1-4880-887d-5a952916d127.mock.pstmn.io"
-    
-    // Link to JSON Server (trocar 'localhost' pelo IP da máquina)
-    // baseURL: "http://localhost:5000"
+  // Link to Postman Mock Server
+  baseURL: "",
+
+  // Link to JSON Server (trocar 'localhost' pelo IP da máquina)
+  // baseURL: "http://localhost:5000"
 });
 
 export const obterAnuncios = async (url, setAnuncios) => {
-    await api.get(url)
-        .then(resultado => {
-            setAnuncios(resultado.data)
-        }).catch((erro) => console.error(erro));
-}
+  await api
+    .get(url)
+    .then((resultado) => {
+      setAnuncios(resultado.data);
+    })
+    .catch((erro) => console.error(erro));
+};
 
 export const obterOfertas = async (url, setOfertas) => {
-    await api.get(url)
-        .then(resultado => {
-            setOfertas(resultado.data)
-        }).catch((erro) => console.error(erro));
-}
+  await api
+    .get(url)
+    .then((resultado) => {
+      setOfertas(resultado.data);
+    })
+    .catch((erro) => console.error(erro));
+};
 
 export const obterProdutos = async (url, setProdutos) => {
-    await api.get(url)
-        .then(resultado => {
-            setProdutos(resultado.data)
-        }).catch((erro) => console.error(erro));
-}
+  await api
+    .get(url)
+    .then((resultado) => {
+      setProdutos(resultado.data);
+    })
+    .catch((erro) => console.error(erro));
+};
